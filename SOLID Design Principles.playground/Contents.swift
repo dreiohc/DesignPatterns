4824a4c48cc6610f9e5b240f3c44b320badc0675
Myron Dulay <dreiohc@live.com>
2021-04-11 19:16:58 +0800

    dependency inversion principle

--- SOLID Design Principles.playground/Contents.swift
+++ SOLID Design Principles.playground/Contents.swift
@@ -249,7 +249,63 @@ class MultiFunctionMachine: MultiFunctionDevice {
   func fax(d: Document) {
     
   }
+}
+
+//: Dependency Inversion Principle
+//: High-level module should not depend on low-level module but both should depend on abstractions. Abstraction should not depend on details but details should depend on abstractions.
+enum Relationship {
+  case parent
+  case child
+  case sibling
+}
+
+class Person {
+  var name = ""
+  init(_ name: String) {
+    self.name = name
+  }
+}
+
+protocol RelationshipBrowser {
+  func findAllChildrenOf(_ name: String) -> [Person]
+}
+
+class Relationships: RelationshipBrowser {
+
+  // low-level module
+  private var relations = [(Person, Relationship, Person)]()
   
+  func addParentAndChild(_ p: Person, _ c: Person) {
+    relations.append((p, .parent, c))
+    relations.append((c, .child, p))
+  }
+  
+  func findAllChildrenOf(_ name: String) -> [Person] {
+    return relations.filter{ $0.name == name && $1 == .parent && $2 === $2 }.map{ $2 }
+  }
+}
+
+class Research { // high-level module
+
+  init(_ browser: RelationshipBrowser) {
+    for p in browser.findAllChildrenOf("John") {
+      print("John has a child \(p.name)")
+    }
+  }
   
 }
+
+func dependencyInversionPrincipleSample() {
+  let parent = Person("John")
+  let child1 = Person("Chris")
+  let child2 = Person("Matt")
   
+  let relationships = Relationships()
+  relationships.addParentAndChild(parent, child1)
+  relationships.addParentAndChild(parent, child2)
+  
+  let _ = Research(relationships)
+}
+
+//dependencyInversionPrincipleSample()
+
